2ca9922d1b003ffadfbc8e92f7292186cb7a87cd
Samy Rabih <contact@lahaut.info>
2017-07-16 13:07:31 +0200

    First commit

---
+++ setup/footer.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('WIKINI_VERSION')) {
+    die("acc&egrave;s direct interdit");
+}
+
+ob_end_flush();
+?>
+  <script src="tools/templates/libs/vendor/jquery-1.11.3.min.js"></script>
+  <script src="tools/templates/libs/vendor/bootstrap.min.js"></script>
+</body>
+</html>
